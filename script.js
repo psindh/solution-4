720eea761284a04c338c0f0ee03380454807309b
psindh <sindhwani143@gmail.com>
2016-07-03 23:11:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var names = ["Yasuo", "Jhin", "Jinx", "Graves", "Poppy", "Fizz", "Lux", "Panthon
 
 
 for (var i in names) {
-  var firstLetter = ((names[i]).charAt(0));
-  if (firstLetter == 'j'||'J') {
+  var firstLetter = ((names[i]).charAt(0).toLowerCase());
+  if (firstLetter == 'j') {
      window.byeSpeaker.speak(names[i]);
   } else {
      window.helloSpeaker.speak(names[i]);
